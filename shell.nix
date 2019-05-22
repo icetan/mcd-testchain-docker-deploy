c879df14b7f45e5d42f8d2b039c24d6bfa9ed7aa
Christopher Fredén <me@icetan.org>
2019-05-22 21:55:15 +0200

    Add comments

--- shell.nix
+++ shell.nix
@@ -1,3 +1,9 @@
+# A Nix function which can be called with the optional argument `tdds-src`,
+# the default value is a derivation which fetches the `master` branch of
+# `testchain-dss-deployment-scripts` from GitHub.
+#
+# By calling this function with `{ tdds-src = /some/local/fs/path; }` you can inject
+# any version of `tdds` when developing.
 { tdds-src ? fetchGit {
     url = https://github.com/makerdao/testchain-dss-deployment-scripts.git;
     ref = "master";
